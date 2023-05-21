c986401d1f52662f1b191b9270528965a217d73f
mazi-samuel <mazizobo@gmail.com>
2023-05-21 22:32:09 +0100

    quote it

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+*/
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	putchar(quote);
+	return (1);
+}
