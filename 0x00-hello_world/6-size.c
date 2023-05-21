badcf804992c9d220cd39aab0e3c9580fd5e0e47
mazi-samuel <mazizobo@gmail.com>
2023-05-21 19:34:10 +0100

    know the size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,9 @@
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of a short: %d byte(s)\n", sizeof(short));
 	printf("Size of a int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long: %d byte(s)\n", sizeof(long));
-	printf("Size of a long long: %d byte(s)\n", sizeof(long long));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	printf("Size of a double: %d byte(s)\n", sizeof(double));
-	printf("Size of a long double: %d byte(s)\n", sizeof(long double));
 	return (0);
 }
