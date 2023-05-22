428f03af0b1d57f97538b67aa6fc650be6890a03
mazi-samuel <mazizobo@gmail.com>
2023-05-22 02:07:20 +0100

    quote it

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include<stdio.h>
 #include<unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Successful)
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	
-	write(2, quote, 59);
+	char qu[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, qu, 59);
 	return (1);
 }
