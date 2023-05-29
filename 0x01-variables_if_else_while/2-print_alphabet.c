387b3c674ea909a1f6d5751a2d7553c33481594b
mazi-samuel <mazizobo@gmail.com>
2023-05-29 20:58:16 +0100

    all in lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i < 26 ; i++)
+	{
+		putchar('a' + i);
+	}
+	putchar('\n');
+	return (0);
+}
