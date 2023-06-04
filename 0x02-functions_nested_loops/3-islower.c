98890142577d3fcdf250465ffb8e74b2d3015a19
mazi-samuel <mazizobo@gmail.com>
2023-06-04 01:29:01 +0100

    checking cases

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * int _islower - checks for upper or lowercase of alphabets
+ * _islower - checks for upper or lowercase of alphabets
+ *
+ * int c - this program takes in c as an argument
  *
  * Return: 1 is lowercase or 0 is uppercase
  */
 int _islower(int c)
 {
 	if (c >= 65 && c <= 90)
-	{
 		return (0);
-	}
-	else (c >= 97 && c <= 112)
-	{
+	else if (c >= 97 && c <= 112)
 		return (1);
-	}
+	return (0);
 }
