9844bb57832b3bfb89bfc753619e13acaa51f585
mazi-samuel <mazizobo@gmail.com>
2023-06-04 19:04:11 +0100

    the time is ticking

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,21 +16,24 @@ void jack_bauer(void)
 
 	for (i = 0; i < 3;)
 	{
-		for (j = 0; j < 4;)
+		for (j = 0; j < 10;)
 		{
-			for (k = 0; k < 6;)
+			if (i <= 2 && (i + j) < 6)
 			{
-				for (l = 0; l < 10; l++)
+				for (k = 0; k < 6;)
 				{
-					_putchar('0' + i);
-					_putchar('0' + j);
-					_putchar(':');
-					_putchar('0' + k);
-					_putchar('0' + l);
-					_putchar('\n');
+					for (l = 0; l < 10; l++)
+					{
+						_putchar('0' + i);
+						_putchar('0' + j);
+						_putchar(':');
+						_putchar('0' + k);
+						_putchar('0' + l);
+						_putchar('\n');
 
+					}
+					k++;
 				}
-				k++;
 			}
 			j++;
 		}
