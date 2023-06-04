dfcc071d23607565d31e7b73fb5b4c98783d6199
mazi-samuel <mazizobo@gmail.com>
2023-06-04 15:09:16 +0100

    the clock is ticking

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * jack_bauer - prints the minute countdown
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i;
+
+	int j;
+
+	int k;
+
+	int l;
+
+	for (i = 0; i < 3;)
+	{
+		for (j = 0; j < 4;)
+		{
+			for (k = 0; k < 6;)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
+					_putchar('0' + l);
+					_putchar('\n');
+
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
