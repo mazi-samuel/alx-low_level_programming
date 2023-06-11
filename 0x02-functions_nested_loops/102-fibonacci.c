4f8daf7053cd94b16394dbe58f769c81ccda22da
mazi-samuel <mazizobo@gmail.com>
2023-06-11 20:38:46 +0100

    fibonacci 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	int b;
+
+	int c;
+
+	a = 1;
+	b = 2;
+
+	for (i = 0; i <= 48; i++)
+	{
+		c = a + b;
+		printf("%i", c);
+		printf("\n");
+		a = b;
+		b = c;
+	}
+	return (0);
+}
