c9449ad0b018ee43851dbbe3388306f28a47d2dc
mazi-samuel <mazizobo@gmail.com>
2023-06-12 14:27:02 +0100

    98 out of many fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long a;
+
+	long b;
+
+	long c;
+
+	int i;
+
+	a = 1;
+	b = 2;
+	printf("%ld, %ld, ", a, b);
+	for (i = 0; i <= 98; i++)
+	{
+		c = a + b;
+		if (i != 98)
+		{
+			printf("%ld, ", c);
+			a = b;
+			b = c;
+		}
+		else
+			printf("%ld", c);
+	}
+	printf("\n");
+	return (0);
+}
