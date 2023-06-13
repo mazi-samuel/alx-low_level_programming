6e423874eff0c44a682cbb6d6b407aaed3541c75
mazi-samuel <mazizobo@gmail.com>
2023-06-13 18:55:32 +0100

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar('0' + i);
+	_putchar('\n');
+}
