837c89808a0fd3d657aaf678db95050bbad613e2
mazi-samuel <mazizobo@gmail.com>
2023-06-15 00:45:09 +0100

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,11 +11,12 @@ void print_line(int n)
 	{
 		int i;
 
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
