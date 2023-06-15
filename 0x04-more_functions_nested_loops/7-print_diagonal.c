cbe5e98719e70a692666996122a723698050c8a9
mazi-samuel <mazizobo@gmail.com>
2023-06-15 16:40:48 +0100

    diagonally

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,15 +15,16 @@ void print_diagonal(int n)
 
 		for (i = 0; i < n; i++)
 		{
-			_putchar(92);
-			_putchar('\n');
-			for (j = 0; j <= (i + 1); j++)
+			for (j = 0; j < n; j++)
 			{
-				_putchar(' ');
+				if (j == i)
+					_putchar(92);
+				else if (j < i)
+					_putchar(' ');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-	else
+	else 
 		_putchar('\n');
 }
