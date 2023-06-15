4c809199b911438a11c980ca3aaf018ecc0e0208
mazi-samuel <mazizobo@gmail.com>
2023-06-15 22:44:02 +0100

    FizzBuzz or i

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,25 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && !(i % 5 == 0))
-			printf("Fizz ");
+		{
+			printf("Fizz");
+			printf(" ");
+		}
 		else if (i % 3 != 0 && i % 5 == 0)
-			printf("Buzz ");
+		{
+			printf("Buzz");
+			printf(" ");
+		}
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
 		else
-			printf("%i ", i);
+		{
+			printf("%i", i);
+			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
