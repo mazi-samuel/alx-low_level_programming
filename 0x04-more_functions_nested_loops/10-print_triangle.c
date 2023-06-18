2a88db51348397a3de0c1132ac2ea8e871e7db92
mazi-samuel <mazizobo@gmail.com>
2023-06-18 01:54:03 +0100

    printed the triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,11 +14,17 @@ void print_triangle(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size - i - 1; j++)
+			{
 				_putchar(' ');
+			}
 			for (k = 0; k <= i; k++)
+			{
 				_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else
+
+		_putchar('\n');
 }
