3123c540ec11e72a98537c9d2b64cfa277081045
mazi-samuel <mazizobo@gmail.com>
2023-06-19 20:13:58 +0100

    highest prime factor

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: a parameter
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n >= 10)
+	{
+		if (!(n >= 100))
+		{
+			_putchar('0' + (n / 10);)
+		}
+	}
+}
