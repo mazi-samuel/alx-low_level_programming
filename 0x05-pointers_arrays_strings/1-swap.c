7952c7666d51694fd1169cf0c18d5bbea893d0da
mazi-samuel <mazizobo@gmail.com>
2023-07-01 15:07:30 +0100

    swipper no swapping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swaps addresses
+ * @a: a parameter
+ * @b: a parameter
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
