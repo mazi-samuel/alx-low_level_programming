b5b8ae8b1cdf1601b63ede6d115755d8fd83474d
mazi-samuel <mazizobo@gmail.com>
2023-07-03 17:28:31 +0100

    integer array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%i", a[i]);
-		printf(",");
-		printf(" ");
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
