3a60c298e92334e55ee89e991d9ba8be86ca2f91
mazi-samuel <mazizobo@gmail.com>
2023-07-03 18:53:42 +0100

    reversing

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,15 +9,13 @@ void rev_string(char *s)
 {
 	int lenght, i;
 
-	char temp;
-
 	lenght = 0;
 
 	while (s[lenght] != '\0')
 		lenght++;
 	for (i = lenght - 1; i >= 0; i--)
 	{
-		_putchar(s[i])
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
