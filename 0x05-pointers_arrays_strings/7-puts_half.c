5954fa1dd45ce237137012478f26f8afb7c3efa7
mazi-samuel <mazizobo@gmail.com>
2023-07-03 13:50:51 +0100

    second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - prints half the charwacters
+ * @str: a parameter
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len, i, n;
+
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 != 0)
+		n = (len - 1) / 2;
+	else
+		n = len / 2;
+	for (i = 0; i <= n; i++)
+		_putchar(str[n + i]);
+	_putchar('\n');
+}
