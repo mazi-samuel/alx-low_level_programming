7f7b3523c546579d9c67e26641a44b5e58273559
mazi-samuel <mazizobo@gmail.com>
2023-07-03 17:22:06 +0100

    integer array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdio.h>
 /**
  * print_array - prints integer arrays
  * @a: a pointer
@@ -11,6 +12,10 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		_putchar(a[i]);
-	_putchar('\n');
+	{
+		printf("%i", a[i]);
+		printf(",");
+		printf(" ");
+	}
+	printf("\n");
 }
