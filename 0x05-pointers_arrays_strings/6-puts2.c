5d412393b100c2df4db843934014bcec3726ab76
mazi-samuel <mazizobo@gmail.com>
2023-07-03 13:19:00 +0100

    increment in twos

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - prints characters in shifts
+ * @str: a pointer
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	for (i = 0; i <= len; i+=2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
