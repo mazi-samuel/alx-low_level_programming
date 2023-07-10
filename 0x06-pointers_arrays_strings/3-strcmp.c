76c5186317b3c77f4bd85f81d9da9c8ce009044b
mazi-samuel <mazizobo@gmail.com>
2023-07-10 19:58:05 +0100

    string compared

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, result;
+	int i;
 
 	i = 0;
-	result = 0;
-	while (s1[i] == s2[i])
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] == '\0')
-			return (0);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
 	}
-	result = *(unsigned char *)s1 - *(unsigned char *)s2;
-	return (result);
+	return (0);
 }
