b09f3b3dd30552388daf3289d51591c4751a8da4
mazi-samuel <mazizobo@gmail.com>
2023-07-13 23:45:13 +0100

    reverse the numbers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * reverse_array - reverses the array content
+ * reverse_array - reverses string order
  * @a: a pointer
- * @n: an integer parameter
+ * @n: n integer
  *
- * Retrun: void
+ * Return: void
  */
 void reverse_array(int *a, int n)
 {
 	int i, t;
-	
+
 	for (i = 0; i < n--; i++)
 	{
 		t = a[i];
 		a[i] = a[n];
 		a[n] = t;
 	}
-		
 }
