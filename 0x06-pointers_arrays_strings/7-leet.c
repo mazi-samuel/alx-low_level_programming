03666294a85d7fc99c049f50475d7736e2bbbec0
mazi-samuel <mazizobo@gmail.com>
2023-07-14 21:20:12 +0100

    encoding it

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * leet - encodes numbers
+ * @n: a pointer
+ *
+ * Return: a character pointer
+ */
+char *leet(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i])
+	{
+		if (n[i] == 'a' || n[i] == 'A')
+			n[i] = '4';
+		else if (n[i] == 'e' || n[i] == 'E')
+			n[i] = '3';
+		else if (n[i] == 'o' || n[i] == 'O')
+			n[i] = '0';
+		else if (n[i] == 't' || n[i] == 'T')
+			n[i] = '7';
+		else if (n[i] == 'l' || n[i] == 'L')
+			n[i] = '1';
+		i++;
+
+	}
+	return (n); 
+}
