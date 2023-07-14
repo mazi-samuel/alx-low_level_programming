026b6aa84edf1e48b7a68d832ad97281dde02eef
mazi-samuel <mazizobo@gmail.com>
2023-07-14 14:29:48 +0100

    changed cases

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper - converts from lowerecase to uppercase
+ * @n: a string pointer
+ * 
+ * Return: returns an uppercase character
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+
+	while (n[i] != '\0')
+	{
+
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+			i++;
+		}
+	}
+	return (n);
+}
