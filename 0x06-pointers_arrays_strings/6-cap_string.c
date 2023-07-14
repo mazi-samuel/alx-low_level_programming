e0f6f536b4bc86743e7c8a0cde151839af62356d
mazi-samuel <mazizobo@gmail.com>
2023-07-14 20:06:17 +0100

    capitalize them

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * cap_string - capitalises words
+ * @n: a pointer to a string
+ *
+ * Return: a pointer to a character
+ */
+char *cap_string(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		while (!(n[i] >= 'a' && n[i] <= 'z'))
+			i++;
+		if (n[i - 1] == ' '  || 
+		    n[i - 1] == '\t' ||
+		    n[i - 1] == '\n' ||
+		    n[i - 1] == ',' ||
+		    n[i - 1] == '.' ||
+		    n[i - 1] == ';' ||
+		    n[i - 1] == '!' ||
+		    n[i - 1] == '?' ||
+		    n[i - 1] == '"' ||
+		    n[i - 1] == '(' ||
+		    n[i - 1] == ')' ||
+		    n[i - 1] == '{' ||
+		    n[i - 1] == '}')
+			n[i] = n[i] - 32;
+		i++;		
+	}
+	return (n);
+}
