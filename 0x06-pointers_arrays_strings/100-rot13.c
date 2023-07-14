dbddb06099f80afce00e5e037012caeeadded42c
mazi-samuel <mazizobo@gmail.com>
2023-07-15 00:57:03 +0100

    encoding strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - encodes strings
+ * @n: a pointer
+ *
+ * Return: a char pointer
+ */
+char *rot13(char *n)
+{
+	int i, j;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	
+	for (i = 0; n[i] != '\0'; i++ )
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (n[i] == input[j])
+			{
+				n[i] == output[j];
+				break:
+			}
+		}
+	}
+	return (n);
+}
