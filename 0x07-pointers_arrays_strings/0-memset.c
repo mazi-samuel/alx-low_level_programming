343bf61e81d57ab0af9a0185aaee9b539b6d7964
mazi-samuel <mazizobo@gmail.com>
2023-07-20 13:33:30 +0100

    were all memes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * _memset fills memory with a constant byte
+ * _memset - fills memory with a constant byte
  * @s: a char pointer
  * @b: a char array
  * @n: an integer parameter
  *
  * Return: returns a pointer to a character
  */
-char *_memset(char *s, char b, unsigned int n) 
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
 	i = 0;
 
-	for (; n > 0; i++) 
+	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
