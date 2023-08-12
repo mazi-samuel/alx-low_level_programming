43f7f7d66d351504bcdca245427920766acdf60e
mazi-samuel <mazizobo@gmail.com>
2023-08-12 14:41:45 +0100

    arg multiplied

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - mulpliplies two numbers recieved as a command line argument
+ * @argc: the number of command line argument
+ * @argv: an array of the commend line argument
+ *
+ * Return: Always 0 successful
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+	else
+		printf("Error\n");
+	return (0);
+	
+}
