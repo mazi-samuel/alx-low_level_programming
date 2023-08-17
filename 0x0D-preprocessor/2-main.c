506d59e29562061e898aa16d55e865f0e38dc2c3
mazi-samuel <mazizobo@gmail.com>
2023-08-17 08:52:48 +0100

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include "_putchar.c"
 /**
  * main - prints the name of file compiled from
  * @argc: the number of command line argument
  * @argv: an array of the command line argumenets
  *
- * Return: Always 0
+ * Return: Always 0 successful
  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
