5d093ee2f33b17c5bf935f5fa750467cfbb6b605
mazi-samuel <mazizobo@gmail.com>
2023-08-19 14:34:40 +0100

    my dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog's basic info
+ * @name: first member of the struct
+ * @age: second member of the struct
+ * @owner: third member of the struct
+ *
+ * Description: Long description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
