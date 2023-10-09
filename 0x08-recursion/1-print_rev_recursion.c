e1ef90b244822970c9d6d7b8869040baf4d67327
mazi-samuel <mazizobo@gmail,com>
2023-10-10 00:57:25 +0100

    reversal

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - writes the character s to stdout
+ * @s: The character to print
+ *
+ * Return: On success 1.
+ */
+void _print_rev_recursion(char *s)
+{
+        if (*s)
+        {
+                _print_rev_recursion(s + 1);
+		_putchar(*s);
+        }
+}
